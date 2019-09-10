7e489fe239b40bf928dd196672ba6107f4846e5f
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-10 17:31:19 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,56 @@
-# Add your code here
+require 'pry'
+
+def get_user_input
+  gets.chomp
+end
+
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  
+  input = get_user_input
+  output = ""
+  
+  songs.each_with_index do |info, index|
+    if input != (index + 1).to_s || input != info
+      puts "Invalid input, please try again"
+    elsif input == (index + 1).to_s || input == info
+      puts output = "Playing #{info}"
+    end
+  end
+  
+  
+end
+
+def list 
+  songs.each_with_index do |info, index|
+    puts (index + 1).to_s + ". " + info
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  
+end
